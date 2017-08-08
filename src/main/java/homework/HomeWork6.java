9dd71f16ebeebc0cf6a1e9050fcfd18c9c66c9f0
mr-joo <skyish912@naver.com>
2017-08-08 09:55:19 +0900

    if에서 else if로 수정 및 비 조건 수정

--- src/main/java/homework/HomeWork6.java
+++ src/main/java/homework/HomeWork6.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class HomeWork6 {
     /*
     학점 계산기
-    95 > A+ / 90 > A / 85 > B+ / 80 > B / 75 > c+ / 70 > c 65 > D 60 <= F
+    95 > A+ / A > 90 / 90 > B+ / 80 > B / 75 > c+ / 70 > c / 65 > D / 60 < F
      */
 
     public static void main (String[] args){
@@ -14,19 +14,19 @@ public class HomeWork6 {
         System.out.printf("점수를 입력하세요 : ");
         int score = scoreScanner.nextInt();
 
-        if (score >= 95){
+        if (95 <= score){
             System.out.printf("A+");
-        } if (score > 90){
+        } else if (90 <= score && 95 > score){
             System.out.printf("A");
-        } if (score >= 85){
+        } else if (85 <= score && 90 > score){
             System.out.printf("B+");
-        } if (score > 80){
+        } else if (80 <= score && 85 > score){
             System.out.printf("B");
-        } if (score > 75){
+        } else if (75 <= score && 80 > score){
             System.out.printf("C+");
-        } if (score > 70){
+        } else if (70 <= score && 75 > score){
             System.out.printf("C");
-        } if (score > 65){
+        } else if (65 <= score && 70 > score){
             System.out.printf("D");
         } else {
             System.out.printf("F");
