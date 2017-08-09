af31599c8d3008109423b04f0ac800aaca186e24
mr-joo <skyish912@naver.com>
2017-08-09 09:57:22 +0900

    문자열에서 입력한 문자 개수 세기 과제 완료

--- src/main/java/homework/HomeWork3.java
+++ src/main/java/homework/HomeWork3.java
@@ -7,20 +7,28 @@ public class HomeWork3 {
     입력된 문자열을 확인한다
     개수를 셀 문자를 선택한다
     문자열을 한 글자씩 읽어들여 내가 입력한 문자와 동일한지 판단
-
      */
 
-    public static void main (String[] args){
+    public static void main(String[] args) {
         String str = "abbccc";
-        //System.out.println("글자를 선택할 문자열 : " +str);
-        //System.out.print("문자열에서 글자를 선택해주세요 : ");
 
-        //Scanner pickText = new Scanner(System.in);
-        //String text = pickText.nextLine();
-
-        for (int i=0; i<str.length(); i++){
+        for (int i = 0; i < str.length(); i++) {
             char tempt = str.charAt(i);
             System.out.println(tempt);
         }
+        Scanner pickChar = new Scanner(System.in);
+        System.out.printf("문자열에서 개수를 셀 문자를 선택해주세요 : ");
+        String pickMe = pickChar.next();
+        char pickedChar = pickMe.charAt(0);
+
+        int count = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            char tempt = str.charAt(i);
+            if (pickedChar == tempt) {
+                count++;
+            }
+        }
+        System.out.printf("입력한 문자의 개수 : " + count);
     }
 }
