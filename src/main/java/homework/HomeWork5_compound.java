229fdf3d830563c9fc342ef317ae923ee24e4715
mr-joo <skyish912@naver.com>
2017-08-13 16:51:52 +0900

    이자 계산기_복리

---
+++ src/main/java/homework/HomeWork5_compound.java
@@ -0,0 +1,31 @@
+package homework;
+
+import java.util.Scanner;
+
+public class HomeWork5_compound {
+    /*
+    원금 이자율 기간(년)을 입력 받는다
+    연 복리 계산
+    */
+    public static void main (String[] args){
+        Scanner inputScanner = new Scanner(System.in);
+
+        System.out.printf("원금을 입력해주세요 : ");
+        double money = inputScanner.nextInt();
+
+        System.out.printf("연이율을 입력해주세요 : ");
+        double interestRate = inputScanner.nextDouble();
+
+        System.out.printf("기간( 년)을 입력해주세요 : ");
+        int period = inputScanner.nextInt();
+
+        double total = money;
+
+        for (int i = 1; i <= period; i++){
+            money = total;
+            double interest = (money * (interestRate/100));
+            total = money + interest;
+        }
+        System.out.printf("총 금액 : " +total);
+    }
+}
