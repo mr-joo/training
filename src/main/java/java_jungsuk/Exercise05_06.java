00ff0b277d1833dbc5fb93a87bd21eb7be039d98
mr-joo <skyish912@gmail.com>
2017-10-30 18:55:42 +0900

    자바의 정석 연습문제

---
+++ src/main/java/java_jungsuk/Exercise05_06.java
@@ -0,0 +1,15 @@
+package java_jungsuk;
+
+public class Exercise05_06 {
+    public static void main(String[] args){
+        int[] coinUnit = {500, 100, 50, 10};
+
+        int money = 2680;
+        System.out.println("money=" + money);
+
+        for (int i = 0; i < coinUnit.length; i++){
+            System.out.println(coinUnit[i] + "원 : " + (money / coinUnit[i]));
+            money = money % coinUnit[i];
+        }
+    }
+}
